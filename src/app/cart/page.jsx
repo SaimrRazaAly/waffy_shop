ab6c4190175296f4719e4763a2f90314f8e95b24
SaimRazaAly <saimrazateach2010@gmail.com>
2025-04-25 21:06:20 -0700

    make cart page responsive

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -1,89 +1,86 @@
-"use client"; // Add this at the top for client-side rendering
+"use client";
 
 import { useContext } from "react";
 import { StoreContext } from "@/context/StoreContext";
-import { food_list } from "@/constants/data"; // Assuming you have a food list with item details
+import { food_list } from "@/constants/data";
 import Image from "next/image";
+import Page_Hero_Banner from "@/Components/Page_Hero_Banner";
+import Plus from "../products/[product_name]/Plus";
+import Border_Btn from "@/Components/Border_Btn";
 
 const CartInfo = () => {
   const { cartItem, getTotalCartAmount, RemoveFromCart } =
     useContext(StoreContext);
 
-  // Calculate total number of items in the cart
-  const totalItems = Object.values(cartItem).reduce((acc, qty) => acc + qty, 0);
-
   return (
-    <div className="cart mt-12 px-4 sm:px-8 lg:px-12 bg-white rounded-lg shadow-lg">
-      <div className="cart-items mb-10">
-        <div className="cart-items-title grid grid-cols-6 gap-4 text-gray-600 text-sm font-medium border-b pb-3">
-          <p>Items</p>
-          <p>Title</p>
-          <p>Price</p>
-          <p>Quantity</p>
-          <p>Total</p>
-          <p>Remove</p>
-        </div>
-        <hr className="bg-gray-200 h-[1px] my-4" />
+    <>
+      <Page_Hero_Banner title="Cart" />
 
-        {food_list.map((v, index) => {
-          if (cartItem[v._id] > 0) {
-            return (
-              <div
-                className="cart-items-title cart-items-item grid grid-cols-6 gap-4 items-center text-gray-800 my-3 p-3 border-b"
-                key={index}
-              >
-                <div>
-                  <div className="relative w-12 h-12">
+      <div className="py-12 mt-12 px-4 sm:px-8 lg:px-12">
+        <h3 className="main_text text-2xl font-semibold mb-6">Products</h3>
 
-                  <Image
-                    src={v.image}
-                    alt={v.name}
-                    fill
-                    className=" object-cover rounded-md"
-                    />
+        <div className="flex flex-col md:flex-row mx-auto gap-6 justify-between px-5">
+          <div className="flex md:basis-[50%] flex-col items-center justify-center gap-6">
+            {food_list.map((v, index) => {
+              if (cartItem[v._id] > 0) {
+                return (
+                  <div
+                    key={index}
+                    className="relative border border-gray-300 rounded-lg shadow-sm p-4 flex flex-col md:flex-row md:gap-5 w-full  items-center bg-white"
+                  >
+                    <div
+                      className="absolute top-2 left-2 w-[30px] h-[30px] rounded-full  text-amber-900 border flex items-center justify-center border-amber-900 font-bold text-lg cursor-pointer hover:border-amber-900 hover:bg-amber-400 transition"
+                      onClick={() => RemoveFromCart(v._id)}
+                    >
+                      X
                     </div>
-                </div>
-                <p className="text-lg font-semibold">{v.name}</p>
-                <p className="text-lg">${v.price}</p>
-                <p className="text-lg">{cartItem[v._id]}</p>
-                <p className="text-lg">${v.price * cartItem[v._id]}</p>
-                <p
-                  className="cross-btn text-red-500 cursor-pointer text-xl font-semibold hover:text-red-600 transition duration-200"
-                  onClick={() => RemoveFromCart(v._id)}
-                >
-                  X
-                </p>
-              </div>
-            );
-          }
-        })}
-      </div>
 
-      <div className="cart-bottom mt-8 flex flex-col sm:flex-row justify-between gap-10 py-10 px-4 bg-gray-100 rounded-lg shadow-md">
-        <div className="cart-total flex flex-col md:gap-6 flex-1">
-          <h2 className="text-xl font-semibold">Cart Total</h2>
-          <div className="cart-total-details flex justify-between text-gray-600">
-            <p>SubTotal</p>
-            <p className="font-medium">${getTotalCartAmount()}</p>
-          </div>
-          <div className="cart-total-details flex justify-between text-gray-600">
-            <p>Delivery Fee</p>
-            <p className="font-medium">${getTotalCartAmount() === 0 ? 0 : 2}</p>
-          </div>
-          <div className="cart-total-details flex justify-between font-bold text-xl">
-            <p>Total</p>
-            <p>${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</p>
-          </div>
+                    <div className="relative w-full h-60 mt-12 mb-4">
+                      <Image
+                        src={v.image}
+                        alt={v.name}
+                        fill
+                        className="object-cover rounded-md"
+                      />
+                    </div>
 
-          <button
-            className="bg-red-500 text-white py-3 rounded-md hover:bg-red-600 transition duration-200 w-full sm:w-[min(15vw,200px)]"
-            onClick={() => navigate("/order")}
-          >
-            PROCEED TO CHECKOUT
-          </button>
+                    <div className="text-center space-y-2">
+                      <p className="text-lg font-semibold">{v.name}</p>
+                      <p className="text-gray-600">${v.price}</p>
+                      <Plus id={v._id} notitle={false} />
+                      <p className="text-base font-medium">
+                        Total: ${v.price * cartItem[v._id]}
+                      </p>
+                    </div>
+                  </div>
+                );
+              }
+            })}
+          </div>
+          {/*  mroe info*/}
+          <div className=" md:my-2 my-10">
+            <div className="flex gap-4 mb-4">
+              <Border_Btn title={"Continue Shoping"} />
+              <Border_Btn title={"Update Cart"} />
+            </div>
+            <div>
+              <h3 className="main_text text-2xl">order summary</h3>
+              <h5 className="main_text text-[16px]  my-2 !font-medium">
+                order summary : ${getTotalCartAmount()}
+              </h5>
+              <Border_Btn title={"Add a note to your order"} />
+            </div>
+            <p className="py-4">
+              shipping ,taxes and dicsounts will be calculated at checkout
+            </p>
+            <div className="flex  justify-center flex-col gap-2">
+              <Border_Btn title={"Proceed to Checkout"} />
+              <Border_Btn title={"Get Shipping estimates"} />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/app/products/[product_name]/Plus.jsx
+++ src/app/products/[product_name]/Plus.jsx
@@ -2,11 +2,11 @@
 import { useContext } from "react";
 import { StoreContext } from "@/context/StoreContext"; // Import context
 
-const Plus = ({ id }) => {
+const Plus = ({ id, notitle = true }) => {
   // Use the context to access cartItem and the functions
   const { cartItem, AddToCart, RemoveFromCart } = useContext(StoreContext);
 
-  return (
+  return notitle ? (
     <div className="flex items-center justify-between">
       <h4 className="text-amber-900 font-bold">Quantity:</h4>
       <div className="flex p-2 border items-center justify-center border-slate-200">
@@ -25,6 +25,23 @@ const Plus = ({ id }) => {
           +
         </button>
       </div>
+    </div>
+  ) : (
+    <div className="flex p-2 border items-center justify-center border-slate-200">
+      <button
+        className="border-r border-slate-200 px-3"
+        onClick={() => RemoveFromCart(id)} // Remove from cart when - button is clicked
+      >
+        -
+      </button>
+      <p className="px-2">{cartItem[id] || 0}</p>{" "}
+      {/* Display current quantity */}
+      <button
+        className="border-l border-slate-200 px-3"
+        onClick={() => AddToCart(id)} // Add to cart when + button is clicked
+      >
+        +
+      </button>
     </div>
   );
 };
