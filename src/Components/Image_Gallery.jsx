5d252323512532117b02359594f93e8a4c7ab543
SaimRazaAly <saimrazateach2010@gmail.com>
2025-05-06 22:15:30 -0700

    Image Gallery

--- src/Components/Image_Gallery.jsx
+++ src/Components/Image_Gallery.jsx
@@ -1,31 +1,15 @@
 "use client";
 
+import Border_Btn from "./Border_Btn";
+import Link from "next/link";
 import Image from "next/image";
 
-import Border_Btn from "./Border_Btn";
-import { Items_data } from "@/constants/data";
-import Link from "next/link";
-
-const Image_Gallery = () => {
-  return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 tlg:grid-cols-6 ">
-      <div className="bg-red-300  p-4 lg:col-span-3 tlg:col-span-2 tlg:row-span-3" />
-      <div className="bg-red-300 p-4   " />
-      <div className="bg-red-300  p-4 lg:row-span-3 tlg:col-span-2 " />
-      <div className="bg-red-300  p-4 " />
-      <div className="bg-red-300  p-4" />
-      <div className="bg-red-300  p-4" />
-    </div>
-  );
-};
-
-export default Image_Gallery;
-
 // ========== HoverCard component ==========
+
 const HoverCard = ({ extraClass }) => {
   return (
     <div
-      className={`relative group overflow-hidden w-full h-[300px]  ${extraClass}`}
+      className={`relative group overflow-hidden w-full h-[300px] ${extraClass}`}
     >
       <Image
         src={"/images/2.jpg"}
@@ -37,7 +21,7 @@ const HoverCard = ({ extraClass }) => {
       {/* Hover Overlay (Left Side) */}
       <div
         className="absolute inset-0 bg-black/70 group-hover:w-[100%] w-0 top-0 left-1/2 transform
-      -translate-x-1/2 flex justify-center items-center text-white transition-all duration-500 ease-in-out"
+        -translate-x-1/2 flex justify-center items-center text-white transition-all duration-500 ease-in-out"
       />
 
       {/* Text and Button */}
@@ -48,10 +32,39 @@ const HoverCard = ({ extraClass }) => {
             Subtitle or small description
           </p>
           <Link href={"/collections/all"}>
-            <Border_Btn title="Learn More" />
+            <Border_Btn title="Learn More" black={false}/>
           </Link>
         </div>
       </div>
     </div>
   );
 };
+
+// ========== Image Gallery ==========
+
+const Image_Gallery = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 tlg:grid-cols-6">
+      <div className="lg:col-span-3 tlg:col-span-2 tlg:row-span-3">
+        <HoverCard extraClass={"lg:!h-[610px]"} />
+      </div>
+      <div>
+        <HoverCard />
+      </div>
+      <div className="lg:row-span-3 tlg:col-span-2">
+        <HoverCard extraClass={"lg:!h-[610px]"} />
+      </div>
+      <div>
+        <HoverCard />
+      </div>
+      <div>
+        <HoverCard />
+      </div>
+      <div>
+        <HoverCard />
+      </div>
+    </div>
+  );
+};
+
+export default Image_Gallery;
